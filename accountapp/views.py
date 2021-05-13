da548237598d7c3e712b142dae9761b386bc58ef
seol <shs00421@gmail.com>
2021-05-13 15:48:03 +0900

    make template

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def hello_world(requset):
-    return HttpResponse('안녕하세요')
+def hello_world(request):
+    return render(request, 'base.html')
